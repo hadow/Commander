8dd4b5168cbe9590f25bab6fe7636bcb1070eeda
Hadow <hadowhadow@gmail.com>
2017-03-21 17:17:02 +0800

    lua modifer

--- RA.Common/Scripting/LuaScript.cs
+++ RA.Common/Scripting/LuaScript.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using RA.Game;
-using RA.Game.Graphics;
-using RA.Game.Scripting;
+using RA;
+using RA.Graphics;
+using RA.Scripting;
 namespace RA.Common.Scripting
 {
     public class LuaScriptInfo:ITraitInfo
